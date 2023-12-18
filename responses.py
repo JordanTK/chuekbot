877fe39529dc31512ee62c65a660e6232c59c417
JordanTK <119076325+JordanTK@users.noreply.github.com>
2023-12-18 20:18:52 +0200

    adding unit tests for valid and invalid inputs.

--- responses.py
+++ responses.py
@@ -1,7 +1,10 @@
 import asyncio
 class GoodMorning:
+    # Would give us better test design
+    def good_morning(self, text):
+        return "добро утро" in text.lower()
     def respond(self,message):
-        if "добро утро" in str(message.content).lower():
+        if self.good_morning(str(message.content)):
              return message.add_reaction("☕")
         else:
             return asyncio.sleep(0)
