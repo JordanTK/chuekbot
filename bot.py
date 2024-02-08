d2cdb50f651e16c64c932fc31f98398930d15671
JordanTK <119076325+JordanTK@users.noreply.github.com>
2024-02-09 00:24:16 +0200

    Issue 5: unit tests

--- bot.py
+++ bot.py
@@ -1,21 +1,33 @@
 import discord,os,responses
 from dotenv import load_dotenv
 
-# Loading the variables from .env file
+# Standard way for loading variables from environment
 load_dotenv()
+
+# Chose to store response procedures in a list and call them sequentially
 responses = [responses.GoodMorning()]
+
+# Function describing the parameters needed to start session
+# Firstly we get the token environment containing the bot unique token that connects to discord servers
+# Secondly we specify the intents, instance of the Intents class- what is our bot purpose and what permissions(access to information) does it have
+# Thirdly we start the session by creating an instance of the client class specfying the intents parameter as the intents variable defined above
 def run_discord_bot():
     TOKEN = os.getenv("token")
     intents = discord.Intents(messages = True, message_content = True, dm_messages = True, reactions = True, emojis = True)
     client = discord.Client(intents=intents)
 
+    # Discord package takes event-driven approach. Here when our session is initiated(ready) we are going to see message
+    # saying the bot is running which is nice letting us know we have succeeded in starting the service
     @client.event
     async def on_ready():
         print(f"{client.user} is now running!")
-    
+
+    # Again following the event-driven approach when the bot detects message(direct or in a channel) the bot looks up in the list
+    # of responses to a message and responds to the message as described in responses.py
     @client.event
     async def on_message(message):
         for response in responses:
                     await response.respond(message=message)
-                     
+                    
+    # Using the token to run the bot on discord servers                 
     client.run(TOKEN)
--- responses.py
+++ responses.py
@@ -1,7 +1,23 @@
+# Need this to make the bot do nothing and wait for a certain period when it should not respond
 import asyncio
+
+# Separating distinct response behaviour in class of its own, in this case class GoodMorning is dealing with response to a
+# message possibly containing good morning greeting
 class GoodMorning:
+
+    # Following test-driven development we do not neeed to test whether discord package adds reactions properly
+    # we are going to trust its fuctionality and test whether the string good morning is present in a text string
+    # then evaluate that with the actual message the bot has detected
+    def good_morning(self, text):
+
+        # check whether good morning in string
+        return "добро утро" in text.lower()
+    
     def respond(self,message):
-        if "добро утро" in str(message.content).lower():
-             return message.add_reaction("☕")
-        else:
-            return asyncio.sleep(0)
+
+        # check whether string good morning is contained within the specific message
+        if self.good_morning(str(message.content)):
+            return message.add_reaction("☕")
+        
+        #do nothing and wait for a certain period when bot should not respond
+        return asyncio.sleep(0)
